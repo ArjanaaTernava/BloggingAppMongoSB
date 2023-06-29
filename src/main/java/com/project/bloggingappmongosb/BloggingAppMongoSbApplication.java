f5599c19644675d0ebe2f26fa6a5cfa3933fca8f
ArjanaaTernava <arjana.ternava@evonem.com>
2023-06-29 17:03:01 +0200

    first commit

---
+++ src/main/java/com/project/bloggingappmongosb/BloggingAppMongoSbApplication.java
@@ -0,0 +1,13 @@
+package com.project.bloggingappmongosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingAppMongoSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingAppMongoSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/bloggingappmongosb/BloggingAppMongoSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bloggingappmongosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingAppMongoSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
